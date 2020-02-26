a06b2d0b92498cdf8321d12329e72b54c1fc9bba
codewarH <44767714+codewarH@users.noreply.github.com>
2020-02-26 16:51:07 +0200

    about page sample filled in

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,38 @@
 import React from "react";
+import HomeTitle from "../components/HomeTitle";
+import Content from "../components/Content";
+import Form from "react-bootstrap/Form";
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null
+    };
+  }
 
-function ContactPage(props) {
-  return <p>ContactPage Works! </p>;
+  render() {
+    return (
+      <div>
+        <HomeTitle title={this.props.title} />
+        <Content>
+          <Form>
+            <Form.Group>
+              <Form.Label htmlFor="full-name"> Full Name </Form.Label>
+              <Form.Control
+                id="full-name"
+                name="name"
+                type="text"
+                value={this.state.name}
+              />
+            </Form.Group>
+          </Form>
+        </Content>
+      </div>
+    );
+  }
 }
 export default ContactPage;
