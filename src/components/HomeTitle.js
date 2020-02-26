c90fb97524c06752560003fd746be2d1128e6ae1
codewarH <44767714+codewarH@users.noreply.github.com>
2020-02-26 12:14:21 +0200

    changes

--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function HomeTitle(props) {
   return (
-    <HomeTitle
-      title={props.title}
-      subTitle={props.subTitle}
-      text={props.text}
-    />
+    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+      <Container fluid={true}>
+        <Row className="justify-content-center py-5">
+          <Col md={0} sm={12}>
+            {props.title && (
+              <h1 className="display-1 font-weight-bolder"> {props.title}</h1>
+            )}
+            {props.subTitle && (
+              <h3 className="display-4 font-weight-light"> {props.subTitle}</h3>
+            )}
+            {props.text && (
+              <h3 className="lead font-weight-light">{props.text}</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,18 @@
 import React from "react";
+
 import HomeTitle from "../components/HomeTitle";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Carousel from "../components/Carousel";
+
 function HomePage(props) {
   return (
-    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
-      <Container fluid={true}>
-        <Row className="justify-content-center py-5">
-          <Col md={0} sm={12}>
-            {props.title && (
-              <h1 className="display-1 font-weight-bolder"> {props.title}</h1>
-            )}
-            {props.subTitle && (
-              <h3 className="display-4 font-weight-light"> {props.subTitle}</h3>
-            )}
-            {props.text && (
-              <h3 className="lead font-weight-light">{props.text}</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </Jumbotron>
+    <div>
+      <HomeTitle
+        title={props.title}
+        subTitle={props.subTitle}
+        text={props.text}
+      />
+      <Carousel />
+    </div>
   );
 }
 export default HomePage;
